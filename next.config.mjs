1bab94c1dada1ca4215fac8d136328e7aa232f2c
bobinstein <cavanaugh.stephen1991@gmail.com>
2024-12-09 17:25:11 -0500

    chore: corrected base

--- next.config.mjs
+++ next.config.mjs
@@ -16,8 +16,7 @@ const withMDX = nextMDX({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: '/docs',
-  assetPrefix: '/docs/',
+  basePath: '/',
   trailingSlash: true,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
    outputFileTracingIncludes: {
