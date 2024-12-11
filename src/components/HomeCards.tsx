10774e91724ac42873e47b7b6dc2fb496c4853cd
bobinstein <cavanaugh.stephen1991@gmail.com>
2024-12-11 10:43:43 -0500

    chore: Moved Home Cards to component

---
+++ src/components/HomeCards.tsx
@@ -0,0 +1,79 @@
+
+
+export default function HomeCards(){
+    return(
+        <div className="HomeContainer">
+            <div id="block-container" className="home-container">
+
+<div className="home-box">
+<a href="./gateways">
+<img className="smaller-image" src="https://arweave.net/7QeMMRmjItuOHFBkbtJkBj43iJLcaG657K0SR8VS6jM" ></img>
+<h3>ar.io Gateways</h3>
+<p>ar.io's modular gateways are built for the Arweave permanent data storage network and optimized for data retrieval, caching, serving and indexing transactions.</p>
+</a>
+</div>
+
+<div className="home-box">
+<a href="./arns">
+<img className="smaller-image" src="https://arweave.net/FR2qSulzK089wjDXhjqKoJ2A58Ojt9VR0TyPrwFUzEo" ></img>
+<h3>ArNS Names</h3>
+<p>ArNS is a censorship-resistant naming system stored on Arweave, enabling user-friendly domain names that link to permaweb dApps, web pages, data, and identities.</p>
+</a>
+</div>
+
+<div className="home-box">
+<a href="./token">
+<img className="smaller-image" src="https://arweave.net/BU8LC66WTUNhok2V4ZjIMS_0JzMNJTgx2XOrra-pL8E" ></img>
+<h3>ARIO Token</h3>
+<p>ARIO powers the AR.IO Network and its suite of permaweb applications as a permissionless and censorship resistant medium of common value for the network.</p>
+</a>
+</div>
+
+<div className="home-box">
+<a href="https://arweave.net/lNjWn3LpyhKC95Kqe-x8X2qgju0j98MhucdDKK85vc4" target="_blank">
+<img className="smaller-image" src="https://arweave.net/_amJG8SHeTBYXiBL3-uIv4JLhOZfjaCTRQ9fbpnzPys" ></img>
+<h3>White Paper</h3>
+<p>A comprehensive document that details a decentralized and incentivized gateway network aimed at making the permaweb more accessible to all.</p>
+</a>
+</div>
+
+<div className="home-box">
+<a href="./arweave">
+<img className="smaller-image" src="https://arweave.net/wa5xjPQcR0cCrhPkB9OSKD6H5t2lfI4JnezjEEksNcc" ></img>
+<h3>The Permaweb</h3>
+<p>Learn more about the Arweave network, the permaweb and the reason the ar.io gateway network was built.</p>
+</a>
+</div>
+
+<div className="home-box">
+<a href="./wayfinder">
+<img className="smaller-image" src="https://arweave.net/qQwoUS6gTQ_9JQa6z1XoTkFGAIKG_4qhF6RuQFVQogE" ></img>
+<h3>ar://</h3>
+<p>The Wayfinder protocol transforms traditional Arweave URLs into more concise and user-friendly forms.</p>
+</a>
+</div>
+
+</div>
+
+<br></br>
+
+<center>
+<h3>ar.io = Arweave Input & Output</h3>
+<div className="video-container">
+<iframe width="760" height="428" src="https://www.youtube.com/embed/6HJc4_OYVMQ" title="What and Why AR.IO? | AR.IO 101" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+</div>
+
+<br></br>
+<br></br>
+
+<h3>Questions? Join our Discord</h3>
+
+<a href="https://discord.gg/7zUPfN4D6g" target="_self" className="discord-container">
+    <img className="discord-logo-home" src="https://arweave.net/Ygo0rdX1H9CmxPxS8fNnezSPRROCDbm0CsJq2U5c43k" alt="Discord Icon" ></img>
+    <span className="discord-button-text">ar.io Discord</span>
+</a>
+
+</center>
+        </div>
+    )
+}
