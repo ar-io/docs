1cd2907f1f5e628f230bd7fa612373eb0116d1fb
bobinstein <cavanaugh.stephen1991@gmail.com>
2024-12-16 11:26:09 -0500

    chore: Added env based basePath

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,7 @@ const withMDX = nextMDX({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: process.env.BASE_PATH || "/",
   trailingSlash: false,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
    outputFileTracingIncludes: {
