4c60c69655f9ae878454abd6dce400ce4f7ca58b
bobinstein <cavanaugh.stephen1991@gmail.com>
2024-12-17 15:47:17 -0500

    feat: Customized the 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,22 +1,30 @@
 import { Button } from '@/components/Button'
 import { HeroPattern } from '@/components/HeroPattern'
+import { SquareArrowOutUpRight } from 'lucide-react'
 
 export default function NotFound() {
   return (
     <>
       <HeroPattern />
       <div className="mx-auto flex h-full max-w-xl flex-col items-center justify-center py-16 text-center">
-        <p className="text-sm font-semibold text-zinc-900 dark:text-white">
-          404
-        </p>
-        <h1 className="mt-2 text-2xl font-bold text-zinc-900 dark:text-white">
-          Page not found
+        <p className="text-lg text-zinc-900 dark:text-white">404s suck!</p>
+        <img src="https://arweave.net/JluJoV__SITJWXvtzkoKvsMoRqQOWnvVX7G6kAj2RdU"></img>
+        <h1 className="mt-2 text-2xl font-semibold text-zinc-900 dark:text-zinc-400">
+          Good thing the permaweb has
         </h1>
-        <p className="mt-2 text-base text-zinc-600 dark:text-zinc-400">
-          Sorry, we couldn’t find the page you’re looking for.
+        <p className="mt-2 text-2xl font-bold text-zinc-600 underline dark:text-white">
+          <a
+            href="https://ardrive.io/fragility-of-the-internet/"
+            target="_blank"
+            rel="noreferrer"
+            className="inline-flex items-center"
+          >
+            {' '}
+            no more 404s <SquareArrowOutUpRight className="w-4 h-4"/>
+          </a>
         </p>
         <Button href="/" arrow="right" className="mt-8">
-          Back to docs
+          Back to Home
         </Button>
       </div>
     </>
