dddd0f3d544ce132b3384130dccd510f6f205fcb
dtfiedler <dylan@ar.io>
2025-09-16 16:25:17 -0500

    chore(docs): update openapi spec for ar-io-node

--- scripts/generate-api-docs.ts
+++ scripts/generate-api-docs.ts
@@ -1,10 +1,21 @@
 // scripts/generate-api-docs.ts
-
-const openApiMap: Record<string, string | Record<string, string>> = {
-  'ar-io-node': 'https://raw.githubusercontent.com/ar-io/ar-io-node/refs/heads/main/docs/openapi.yaml',
-  // 'turbo': {
-  //   'upload-service': 'https://raw.githubusercontent.com/ardriveapp/upload-service/refs/heads/main/docs/openapi.yaml',
-  //   'payment-service': 'https://raw.githubusercontent.com/ardriveapp/payment-service/refs/heads/main/docs/openapi.yaml',
+const openApiMap: Record<string, {
+  openapi: string;
+  name: string;
+}> = {
+  'ar-io-node': {
+    name: 'AR.IO Node',
+    openapi: 'https://raw.githubusercontent.com/ar-io/ar-io-node/refs/heads/openapi-update/docs/openapi.yaml',
+  },
+  // 'turbo/upload-service': {
+  //   name: 'Turbo Upload Service',
+  //   openapi: 'https://raw.githubusercontent.com/ardriveapp/upload-service/refs/heads/main/docs/openapi.yaml',
+  //   proxyUrl: 'https://ardrive.net',
+  // },
+  // 'turbo/payment-service': {
+  //   name: 'Turbo Payment Service',
+  //   openapi: 'https://raw.githubusercontent.com/ardriveapp/payment-service/refs/heads/main/docs/openapi.yaml',
+  //   proxyUrl: 'https://ardrive.net',
   // },
 };
 
@@ -14,33 +25,21 @@ const { createOpenAPI } = await import('fumadocs-openapi/server');
 
   for (const [key, openapi] of Object.entries(openApiMap)) {
     // if open api map is a string, use it as the input, if it's nested then the key is the top level path and the nested key is the subpath
-    if (typeof openapi === 'string') {
+    const openApi = createOpenAPI({
+      input: [openapi.openapi],
+      disableCache: true,
+    });
     await generateFiles({
-      input: createOpenAPI({ 
-        input: [openapi],
-      }),
+      input: openApi,
       output: `./content/api/${key}/`,
       includeDescription: true,
       per: 'tag',
       frontmatter: (title) => {
         // Fix title capitalization
         if (title.toLowerCase() === 'ar n s') return { title: 'ArNS' };
-        if (title.toLowerCase() === 'ar-io') return { title: 'AR.IO' };
+        if (title.toLowerCase() === 'ar io') return { title: 'AR.IO' };
         return { title };
       },
     });
-    } else {
-      for (const [nestedKey, nestedOpenapi] of Object.entries(openapi)) {
-        const openApi = createOpenAPI({ 
-          input: [nestedOpenapi],
-        });
-        await generateFiles({
-          input: openApi,
-          output: `./content/api/${key}/${nestedKey}/`,
-          includeDescription: true,
-          per: 'tag',
-        });
-      }
-    }
   }
 })();
