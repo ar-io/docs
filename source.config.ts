e0ffbccc59aad833395af805ad2bfec4d524f4ca
bobinstein <cavanaugh.stephen1991@gmail.com>
2025-09-18 15:23:23 -0400

    docs: oip docs before formula change

--- source.config.ts
+++ source.config.ts
@@ -4,8 +4,8 @@ import {
   frontmatterSchema,
   metaSchema,
 } from "fumadocs-mdx/config";
-import { remarkMdxMermaid } from 'fumadocs-core/mdx-plugins';
 
+import { remarkMdxMermaid } from "fumadocs-core/mdx-plugins";
 
 // You can customise Zod schemas for frontmatter and `meta.json` here
 // see https://fumadocs.dev/docs/mdx/collections#define-docs
