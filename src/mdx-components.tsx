94c6d9a0177faf746489ca61e5047fe7348add56
bobinstein <cavanaugh.stephen1991@gmail.com>
2025-09-19 15:09:43 -0400

    docs: refactor the upload section

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,11 +1,11 @@
-import defaultMdxComponents from 'fumadocs-ui/mdx';
-import type { MDXComponents } from 'mdx/types';
-import Tip from '@/components/Tip';
-import { CodeGroup } from '@/components/Code';
-import Mermaid from '@/components/Mermaid';
-import { openapi } from '@/lib/openapi';
-import { APIPage } from 'fumadocs-openapi/ui';
-import { Step, Steps } from 'fumadocs-ui/components/steps';
+import defaultMdxComponents from "fumadocs-ui/mdx";
+import type { MDXComponents } from "mdx/types";
+import Tip from "@/components/Tip";
+import { CodeGroup } from "@/components/Code";
+import Mermaid from "@/components/Mermaid";
+import { openapi } from "@/lib/openapi";
+import { APIPage } from "fumadocs-openapi/ui";
+import { Step, Steps } from "fumadocs-ui/components/steps";
 import {
   FolderOpen,
   Info,
@@ -17,6 +17,9 @@ import {
   Globe,
   Search,
   Link,
+  CreditCard,
+  Upload,
+  Tag,
 } from "lucide-react";
 
 // use this function to get MDX components, you will need it for rendering MDX
@@ -38,6 +41,9 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
     Globe,
     Search,
     Link,
+    CreditCard,
+    Upload,
+    Tag,
     APIPage: (props) => <APIPage {...openapi.getAPIPageProps(props)} />,
     ...components,
   };
